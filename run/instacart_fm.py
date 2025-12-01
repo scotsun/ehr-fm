d7ffed939c5157ca387761e963fa112c88b71eeb
Scott Sun <minghuisunscott@gmail.com>
2025-12-01 16:04:44 -0500

    update run fiile

---
+++ run/instacart_fm.py
@@ -0,0 +1,127 @@
+import os
+import sys
+from pathlib import Path
+import torch
+import torch.distributed as dist
+import argparse
+
+from torch.optim import AdamW
+from torch.nn import CrossEntropyLoss
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.utils.data import DataLoader, random_split, DistributedSampler
+
+from tokenizers import Tokenizer
+from setproctitle import setproctitle
+
+sys.path.insert(0, str(Path(__file__).parent.parent))
+
+from src.utils.data_utils import SeqSet
+from src.trainer import BaseTrainer, EarlyStopping
+from src.models import FMConfig
+from src.models.base import FMBase
+from src.utils.model_utils import build_model
+from src.utils.train_utils import (
+    load_cfg,
+    setup_training,
+    make_fmbase_signature,
+    setup_mlflow_tracked_fit,
+)
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description="Instacart FM")
+    parser.add_argument("--backend-uri", type=str, default="./mlruns")
+    parser.add_argument("--experiment-name", type=str)
+    parser.add_argument("--run-name", type=str, default=None)
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = get_args()
+    MLFLOW_URI = args.backend_uri
+    EXPERIMENT_NAME = args.experiment_name
+    RUN_NAME = args.run_name
+
+    os.environ["OMP_NUM_THREADS"] = "1"
+    local_rank, world_size, rank, device, is_distributed = setup_training()
+    setproctitle(f"instacart-fm-pretrain-{local_rank}")
+    print(f"local_rank: {local_rank}")
+    print(f"process {rank}/{world_size} using device {device}\n")
+
+    cfg_dict = load_cfg("./configs/instacart_fm.yaml")
+    tk = Tokenizer.from_file(f"./{cfg_dict['model']['tokenizer']}")
+
+    cfg = FMConfig(
+        vocab_size=tk.get_vocab_size(),
+        dataset=cfg_dict["dataset"],
+        trainer=cfg_dict["trainer"],
+        **cfg_dict["model"],
+    )
+    model = build_model(cfg, FMBase, device)
+
+    signature = make_fmbase_signature(cfg)
+
+    trainer = BaseTrainer(
+        model=model,
+        tokenizer=tk,
+        optimizer=AdamW(model.parameters(), lr=cfg.trainer["lr"]),
+        criterions={"cross_entropy": CrossEntropyLoss(ignore_index=-100)},
+        early_stopping=EarlyStopping(
+            patience=cfg.trainer["early_stopping_patience"],
+            mode=cfg.trainer["early_stopping_mode"],
+        ),
+        verbose_period=1,
+        device=device,
+        model_signature=signature,
+        trainer_args=cfg.trainer,
+    )
+
+    if is_distributed:
+        trainer.model = DDP(model, device_ids=[rank])
+
+    kwargs = cfg_dict["dataset"]
+    kwargs["time_operation"] = lambda x: x["t"]
+    instacart = SeqSet(
+        tokenizer=tk,
+        data_folder="/".join([cfg.dataset["data_root"], "instacart.parquet"]),
+        downstream_task_cohort=None,
+        outcome_vars=None,
+        **kwargs,
+    )
+    train, valid = random_split(
+        dataset=instacart,
+        lengths=cfg.trainer["split"],
+        generator=torch.Generator().manual_seed(42),
+    )
+    train_loader = DataLoader(
+        dataset=train,
+        batch_size=cfg.trainer["batch_size"],
+        sampler=DistributedSampler(train),
+        num_workers=8,
+    )
+    valid_loader = DataLoader(
+        dataset=valid,
+        batch_size=cfg.trainer["batch_size"],
+        sampler=DistributedSampler(valid),
+        num_workers=8,
+    )
+
+    setup_mlflow_tracked_fit(
+        mlflow_uri=MLFLOW_URI,
+        experiment_name=EXPERIMENT_NAME,
+        run_name=RUN_NAME,
+        rank=rank,
+        is_distributed=is_distributed,
+        cfg=cfg,
+        trainer=trainer,
+        train_loader=train_loader,
+        valid_loader=valid_loader,
+    )
+
+    if is_distributed:
+        dist.destroy_process_group()
+
+
+if __name__ == "__main__":
+    main()
--- src/utils/model_utils.py
+++ src/utils/model_utils.py
@@ -1,5 +1,8 @@
 import torch
 
+from transformers import PreTrainedModel, PretrainedConfig
+from typing import Type
+
 
 def is_model_half(model: torch.nn.Module):
     """Check if the model is half precision"""
@@ -7,3 +10,12 @@ def is_model_half(model: torch.nn.Module):
         if param.dtype != torch.half:
             return False
     return True
+
+
+def build_model(
+    cfg: PretrainedConfig, model_class: Type[PreTrainedModel], device: str
+) -> PreTrainedModel:
+    """Build the model"""
+    model = model_class(cfg)
+    model.to(device)
+    return model
